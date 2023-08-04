31b1610e6cfad8e76fa0ef4d46bda56a46006938
Jejoon Ryu <rjejoon@gmail.com>
2023-08-03 18:25:20 -0600

    Finish 409 longest palindrome

---
+++ solutions/409_longest_palindrome.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        s_count = Counter(s)
+        odd_count = 0
+        for ch in s_count:
+            if s_count[ch] % 2 == 1:
+                odd_count += 1
+        if odd_count == 0:
+            return len(s)
+        return len(s) - odd_count + 1
+
+
+if __name__ == '__main__':
+    s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+    sol = Solution()
+    expected = 981
+    actual = sol.longestPalindrome(s)
+    assert actual == expected
